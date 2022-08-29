298a81e4f679d14784a3fedeea6d33fd5ff8dd57
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-08-28 23:18:45 -0300

    8th commit (bug fixes + clean code)

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,34 @@
 const cards = [
-	"src/images/3parrots.gif",
-	"src/images/birthdayparrot.gif",
-	"src/images/brazilparrot.gif",
-	"src/images/detectiveparrot.gif",
-	"src/images/devilparrot.gif",
-	"src/images/dogeparrot.gif",
-	"src/images/drinkingparrot.gif",
-	"src/images/footballparrot.gif",
-	"src/images/headbangingparrot.gif",
-	"src/images/lsdparrot.gif",
-	"src/images/mustacheparrot.gif",
-	"src/images/negativeparrot.gif",
-	"src/images/pirateparrot.gif",
-	"src/images/policeparrot.gif",
-	"src/images/popcornparrot.gif",
-	"src/images/scientistparrot.gif",
-	"src/images/soccerparrot.gif",
-	"src/images/sunglassparrot.gif",
-	"src/images/tenisparrot.gif",
-	"src/images/unicornparrot.gif",
+	"3parrots",
+	"birthdayparrot",
+	"brazilparrot",
+	"detectiveparrot",
+	"devilparrot",
+	"dogeparrot",
+	"drinkingparrot",
+	"footballparrot",
+	"headbangingparrot",
+	"lsdparrot",
+	"mustacheparrot",
+	"negativeparrot",
+	"pirateparrot",
+	"policeparrot",
+	"popcornparrot",
+	"scientistparrot",
+	"soccerparrot",
+	"sunglassparrot",
+	"tenisparrot",
+	"unicornparrot",
 ];
 
 const suffledCards = [];
 
 let firstCard = null;
-let moves = 0,
-	totalMoves = 0,
-	wins = 0,
-	id = 0;
-	seconds = 0;
+let moves = 0, totalMoves = 0, wins = 0, id = 0, seconds = 0;
 
-let cardsCount = prompt("Com quantas cartas quer jogar? (Números pares entre 4 e 14)");
+let cardsCount = Number(prompt("Com quantas cartas quer jogar? (Números pares entre 4 e 40)"));
 
-while (cardsCount < 4 || cardsCount > 14 || cardsCount % 2 !== 0) {
+while (cardsCount < 4 || cardsCount > 40 || cardsCount % 2 !== 0) {
 	cardsCount = prompt("Com quantas cartas quer jogar? (Números pares entre 4 e 14)");
 }
 
@@ -46,40 +42,40 @@ function suffleCards() {
 }
 
 function randomCard() {
-	return Math.random() - 0.5;
+	return Math.random() - 0.75;
 }
 
 suffleCards();
 
-function Timer() {
+function setTimer() {
 	seconds++;
 	document.querySelector(".time").innerHTML = seconds;
 }
 
 function distributeCards() {
-	const cards = document.querySelector(".cards");
-	cards.innerHTML = "";
+	const distributedCards = document.querySelector(".cards");
+	distributedCards.innerHTML = "";
 
 	for (let i = 0; i < cardsCount; i++) {
-		cards.innerHTML += `
+		distributedCards.innerHTML += `
 		<div class="card" onclick="checkCard(this)">
-		<div class="face front-face"><img src="src/images/front.png"></div>
-		<div class="face back-face"><img src="${suffledCards[i]}"></div>
+			<div class="face front-face">
+				<img src="src/images/front.png">
+			</div>
+			<div class="face back-face">
+				<img class="gif-parrot" src="src/images/${suffledCards[i]}.gif">
+			</div>
 		</div>
 		`;
 	}
 
-	id = setInterval(Timer, 1000);
+	id = setInterval(setTimer, 1000);
 }
 
 distributeCards();
 
-function blockGame() {
-	document.querySelector(".cards").classList.add("block");
-}
-
 function checkCard(card) {
-	if (card.querySelector(".front-face").classList.contains("front-click") === false && document.querySelector(".cards").classList.contains("block") === false) {
+	if (card.classList.contains("flipped") === false && card.parentElement.classList.contains("block") === false) {
 		if (moves < 1) {
 			turnCard(card);
 			moves++;
@@ -95,19 +91,18 @@ function checkCard(card) {
 }
 
 function compareCard(card1, card2) {
-	if (card1.querySelector(".back-face").querySelector("img").getAttribute("src") === card2.querySelector(".back-face").querySelector("img").getAttribute("src")) {
+	if (card1.querySelector(".gif-parrot").getAttribute("src") === card2.querySelector(".gif-parrot").getAttribute("src")) {
 		wins++;
 		setTimeout(checkWinner, 500);
 	} else {
-		blockGame();
+		document.querySelector(".cards").classList.add("block");
 		setTimeout(turnCard, 1000, card1);
 		setTimeout(turnCard, 1000, card2);
 	}
 }
 
 function turnCard(card) {
-	card.querySelector(".front-face").classList.toggle("front-click");
-	card.querySelector(".back-face").classList.toggle("back-click");
+	card.classList.toggle("flipped");
 	document.querySelector(".cards").classList.remove("block");
 }
 
@@ -116,13 +111,10 @@ function checkWinner() {
 		clearInterval(id);
 		alert(`Parabéns, você venceu em ${totalMoves} jogadas! e levou ${seconds} segundos!`);
 
-		let playAgain = prompt(`Você deseja joga novamente? sim ou não?`);
-		while (playAgain !== "sim" && playAgain !== "não") {
-			playAgain = prompt(`Você deseja joga novamente? sim ou não?`);
-		}
-		if (playAgain === "sim") {
+		let playAgain = confirm(`Você deseja joga novamente?`);		
+		if (playAgain) {
 			window.location.reload();
-		} else if (playAgain === "não") {
+		} else {
 			alert("Obrigado por jogar!");
 		}
 	}
